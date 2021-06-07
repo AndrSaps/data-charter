7be253794b1e58424ee4813a43a2bf7cb70f3e9f
Andrii Mykolyshyn <andrsaps@gmail.com>
2021-06-07 21:47:33 +0300

    initial

---
+++ server/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from constants import *
+from config import *
+from models import db
+
+
+#app configuration
+app = Flask(__name__)
+api = Api(app)
+
+
+#database configuration
+app.config[SQL_DATABASE_URL_LABEL] = DATABASE_URL
+db.init_app(app)
+
+
+#controllers
+class ClasterModel(Resource):
+    def get(self):
+        return {'1':'Hello World!'}
+
+
+
+api.add_resource(ClasterModel, "/trend")
+
+if __name__ == '__main__':
+    app.run()
